c623b15c8d98b5609038587960ce2c69edaae5f6
Lucía <lucia.manso@live.u-tad.com>
2025-01-01 17:18:26 +0100

    JSON stringify

--- app/tecnologias/page.tsx
+++ app/tecnologias/page.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 import tecnologias from '@/app/data/tecnologias.json';
 
 export default function page() {
+
+  // Change from JSON to string
+  const technologiesString = JSON.stringify(tecnologias, null, 2);
+
   return (
-    <div>Tecnologias</div>
-  )
+    <div style={{ padding: '16px' }}>
+      <h1>Tecnologías Aprendidas</h1>
+      {/* Renderizado en formato legible */}
+      <pre style={{ backgroundColor: '#f4f4f4', padding: '16px', borderRadius: '8px' }}> {technologiesString}
+      </pre>
+    </div>
+  );
 }

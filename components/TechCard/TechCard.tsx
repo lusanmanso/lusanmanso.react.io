32be7d6bda2053a469c21775979f2cdd09b1ef71
Lucía <lucia.manso@live.u-tad.com>
2025-01-01 21:11:15 +0100

    another :(

--- components/TechCard/TechCard.tsx
+++ components/TechCard/TechCard.tsx
@@ -1,5 +1,5 @@
-/* /components/Card/Card.tsx */
 import React from 'react';
+import Image from 'next/image';
 
 interface TechCardProps {
     title: string;
@@ -8,27 +8,27 @@ interface TechCardProps {
     rating: number;
 }
 
-import { useState } from 'react';
-
-const TechCard: React.FC<TechCardProps> = ({ title, image,description, rating}) => {
+const TechCard: React.FC<TechCardProps> = ({ title, image, description, rating }) => {
     return (
         <div className="bg-white border border-gray-200 rounded-lg shadow-lg overflow-hidden transform transition hover:scale-105 hover:shadow-2xl">
             <div className="w-full h-32 flex items-center justify-center mt-4">
-                <img
+                <Image
                     src={image}
                     alt={title}
+                    width={150}
+                    height={150}
                     className="max-w-full max-h-full object-contain"
                 />
             </div>
             <div className="p-4">
                 <h2 className="text-lg font-bold text-gray-800">{title}</h2>
                 <p className="text-lg font-semibold text-red-600">{rating.toFixed(2)} ★</p>
-               {/* Description */}
-               <p
-                  className={`text-sm text-gray-600 mt-2 text-justify transition-all duration-300 ease-in-out`}
-               >  
-                  {description}
-                </p>
+                {/* Description */}
+                {description && (
+                    <p className="text-sm text-gray-600 mt-2 text-justify transition-all duration-300 ease-in-out">
+                        {description}
+                    </p>
+                )}
             </div>
         </div>
     );

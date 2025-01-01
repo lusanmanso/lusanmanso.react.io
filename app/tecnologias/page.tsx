e533f279ac079affe32667424e2c01d0c702a53a
lusanmanso <lusanmanso@gmail.com>
2025-01-01 20:50:50 +0100

    tech restructure

--- app/tecnologias/page.tsx
+++ app/tecnologias/page.tsx
@@ -1,28 +1,20 @@
-'use client'
 import React from 'react';
-
-// Importa el JSON
-import'@/data/tecnologias.json';
-
-import { Technology } from '@/models/interface';
+import tecnologias from '@/app/data/tecnologias.json';
 import TechCard from '@/components/TechCard/TechCard';
 
 export default function Technologies() {
-    // Usa los datos directamente desde el JSON
-    const products: Technology[] = technologies;
-
     return (
         <div className="w-[90%] h-[calc(100vh-4rem)] sm:h-[calc(100vh-5rem)] mx-auto overflow-y-scroll bg-transparent">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-4">
-            {products.map((product) => (
-                <TechCard
-                    key={product.id}
-                    title={product.title}
-                    image={product.image}
-                    rating={product.rating}
-                    description={product.description}
-                />
-            ))}
+                {tecnologias.map((tech) => (
+                    <TechCard
+                        key={tech.id}
+                        title={tech.title}
+                        image={tech.image}
+                        rating={tech.rating}
+                        description={tech.description}
+                    />
+                ))}
             </div>
         </div>
     );

a9e2650ce0f6dc9a46971230b740b98834fbc12c
Lucía <lucia.manso@live.u-tad.com>
2025-01-01 19:12:17 +0100

    readingClamp corrected

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -9,7 +9,10 @@ interface ProductCardProps {
     description?: string;
 }
 
+import { useState } from 'react';
+
 const ProductCard: React.FC<ProductCardProps> = ({ title, category, image, price, description }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
     return (
         <div className="bg-white border border-gray-200 rounded-lg shadow-lg overflow-hidden transform transition hover:scale-105 hover:shadow-2xl">
             <img
@@ -21,7 +24,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ title, category, image, price
                 <h2 className="text-lg font-bold text-gray-800">{title}</h2>
                 <p className="text-sm text-gray-500">{category}</p>
                 <p className="text-lg font-semibold text-green-600">${price.toFixed(2)}</p>
-               {/* Descripción */}
+               {/* Description */}
                <p
                   className={`text-sm text-gray-600 mt-2 text-justify ${
                   isExpanded ? '' : 'line-clamp-3'
@@ -30,12 +33,12 @@ const ProductCard: React.FC<ProductCardProps> = ({ title, category, image, price
                   {description}
                 </p>
 
-               {/* Botón para expandir/cerrar */}
+               {/*Button to close/expand the description */}
                <button
                   onClick={() => setIsExpanded(!isExpanded)}
                   className="text-blue-500 hover:underline mt-2"
                >
-                  {isExpanded ? 'Leer menos' : 'Leer más'}
+                  {isExpanded ? 'Read less' : 'Read more'}
                </button>
             </div>
             <div className="px-4 pb-4">
